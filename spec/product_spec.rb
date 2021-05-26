9e5166466762d1eb4a22643ac003b51ebc658446
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-27 01:44:18 +0900

    Productクラスのテストをファクタリング

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,27 +1,22 @@
 RSpec.describe Product do
   describe ".initialize" do
     context "インスタンスが生成されたとき" do
+      let(:product_params) { { name: "じゃがいも", price: 100 } }
+      let(:product) { Product.new(product_params) }
+      let!(:base_id) { Product.class_variable_get("@@count") }
       it "@@countが1増加すること" do
-        product_params = { name: "じゃがいも", price: 100 }
         expect { Product.new(product_params) }.to change { Product.class_variable_get("@@count") }.by(1)
       end
 
       it "商品の@idが、インスタンス生成前の@@countに+1した値と等しいこと" do
-        base_id = Product.class_variable_get("@@count")
-        product_params = { name: "じゃがいも", price: 100 }
-        product = Product.new(product_params)
         expect(product.id).to eq base_id + 1
       end
 
       it "商品の@nameが、product_paramsの名前と等しいこと" do
-        product_params = { name: "じゃがいも", price: 100 }
-        product = Product.new(product_params)
         expect(product.name).to eq "じゃがいも"
       end
 
       it "商品の@priceが、product_paramsの金額と等しいこと" do
-        product_params = { name: "じゃがいも", price: 100 }
-        product = Product.new(product_params)
         expect(product.price).to eq 100
       end
     end
