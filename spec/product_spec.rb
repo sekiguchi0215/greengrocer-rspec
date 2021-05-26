94ab07174ec72a7de5c967f4efe8dfe61485ad0d
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-27 01:36:28 +0900

    Productクラスのテスト完了

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -7,12 +7,22 @@ RSpec.describe Product do
       end
 
       it "商品の@idが、インスタンス生成前の@@countに+1した値と等しいこと" do
+        base_id = Product.class_variable_get("@@count")
+        product_params = { name: "じゃがいも", price: 100 }
+        product = Product.new(product_params)
+        expect(product.id).to eq base_id + 1
       end
 
       it "商品の@nameが、product_paramsの名前と等しいこと" do
+        product_params = { name: "じゃがいも", price: 100 }
+        product = Product.new(product_params)
+        expect(product.name).to eq "じゃがいも"
       end
 
       it "商品の@priceが、product_paramsの金額と等しいこと" do
+        product_params = { name: "じゃがいも", price: 100 }
+        product = Product.new(product_params)
+        expect(product.price).to eq 100
       end
     end
   end
