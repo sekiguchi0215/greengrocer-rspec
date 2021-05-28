7a75ebc1a7c3ce6b652403d7972cf6aa6029c54b
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-28 22:36:18 +0900

    Userクラスのテスト 一時中断

---
+++ spec/user_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe User do
+  describe ".choose_product" do
+    context "存在するid(productsの最初の要素のid)を入力したとき" do
+      it "@chosen_productのidが、productsの最初のidと等しいこと" do
+      end
+      it "@chosen_productの名前が、productsの最初の要素の名前と等しいこと" do
+        
+      end
+      it "@chosen_productの金額が、productsの最初の要素の金額と等しいこと" do
+        
+      end
+    end
+  end
+end
